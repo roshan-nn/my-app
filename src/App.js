6d8b6b300018644483b82fa233e71d16b21548fd
roshan-nn <roshan.thomas767@gmail.com>
2025-12-10 17:51:47 +0530

    usestate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React,{Component} from "react";                          
+import React,{Component, useState} from "react";                          
 import logo from './logo.svg';     
-import Todo from "./components/Todo";      
+    
 
 import './App.css';
-import Header from './components/Header';   
+import Header from './components/Header';      
 
 function App() {
-  
+  const [value, setValue] = useState("hari");
+  const hari= function(){
+    setValue("hari");
+  }
+
+  const royal=function(){
+    setValue("royal");
+  }
+
+  const roshan=function(){
+    setValue("roshan");
+  }
+  return (
+     <div>
+        <h1> {value} </h1> 
+        <div>
+            <button onClick={hari}>Hari</button>
+            <button onClick={royal}>Royal</button>
+            <button onClick={roshan}>Roshan</button>
+
+        </div>
+
+     </div>
+
+  )
+
 }
 export default App;                         
